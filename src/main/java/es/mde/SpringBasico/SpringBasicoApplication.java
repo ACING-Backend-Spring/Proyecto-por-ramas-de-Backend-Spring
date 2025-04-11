32997a2a6477a6ccbee516f86d05e1cbb0d5a6cf
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-04-11 20:41:27 +0200

    ConfiguracionPorJava (@Configuration y @Bean)

--- src/main/java/es/mde/SpringBasico/SpringBasicoApplication.java
+++ src/main/java/es/mde/SpringBasico/SpringBasicoApplication.java
@@ -14,17 +14,18 @@ public class SpringBasicoApplication {
 //		SpringApplication.run(SpringBasicoApplication.class, args);
 
 		ConfigurableApplicationContext context = // mi objeto contenedor
-				new ClassPathXmlApplicationContext(new String[] {
-						"config.xml" // aqui ponemos los archivos de configuracion
-								,
-						"config-scan.xml" });
+				new ClassPathXmlApplicationContext(new String[] { "config.xml" // aqui ponemos los archivos de
+																				// configuracion
+						, "config-scan.xml" });
 		ObjetoTipo miObjetoTipo = new ObjetoTipo();
 		miObjetoTipo.init();
-		miObjetoTipo = context.getBean("xml",ObjetoTipo.class);
+		miObjetoTipo = context.getBean("xml", ObjetoTipo.class);
 		miObjetoTipo.init();
 //		miObjetoTipo = context.getBean("anotacion1",ObjetoTipo.class);
 //		miObjetoTipo.init();
-		miObjetoTipo = context.getBean("anotacion2",ObjetoTipo.class);
+		miObjetoTipo = context.getBean("anotacion2", ObjetoTipo.class);
+		miObjetoTipo.init();
+		miObjetoTipo = context.getBean("config", ObjetoTipo.class);
 		miObjetoTipo.init();
 
 		context.close();
